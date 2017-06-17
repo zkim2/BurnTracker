d011185431f33d293cee23f22316f8d4b0b34613
Zachary Kim <zkim2@illinois.edu>
2017-06-17 13:06:05 -0500

    Main loop, organization, and data graph
    Made code more readable, added main and sub loops, implemented weight
    visualization, and commented a list of features I want to add.

--- WeightLossTracker/WeightLossTracker.py
+++ WeightLossTracker/WeightLossTracker.py
@@ -6,7 +6,27 @@ import matplotlib.pyplot as plt
 
 """
 Author: Zachary Kim
+
+Weight Loss Tracker 
+
 email: zkim2@illinois.edu
+
+
+
+Features I need to implement:
+
+
+-Give user a summary of their diet plan after creating a new profile.
+-Calculation of carbohydrates, fats, and proteins for each profile.
+-Visualization of dietary and exercise programs for each profile. (add human body graphic that gets skinnier proportional to user's weight loss)
+-Each day, users will input weight and caloric intake (later on exercise) - caloric intake stored as dictionary too?
+-Add more graph data such as calories consumed per week (data gained from the above added feature)
+-In order to prevent weight plateau's from users, we have to recalculate their BMR and adjust their deficit according to their weight loss.
+ (we can perform this operation every time the user inputs their weight and caloric intake data everyday)
+-Allow users to email an address or text to send in data which is easier than logging into the computer everyday.
+-Make this script an executable if possible. 
+-Eventually transition from command line to GUI.
+
 """
 
 class Profile:
@@ -57,107 +77,175 @@ class Profile:
 #need to store dates as datetime object so I can sort them when graphing
 currentDate = datetime.date.today()
 
-newName = str(input('What is your full name?: '))
+mainLoopRun = True
 
-newName = newName.replace(' ' , '')
+while mainLoopRun:
 
-newName = newName.lower()
+	profileLoop = True
 
+	#handles the case when user types profile name incorrectly.
+	while profileLoop:
+		newName = str(input('What is your full name?: '))
 
-#finds the pickled file in project directory
-file_name = newName.lower() + '.pickle'
+		#can handle capitalization and spaces
+		newName = newName.replace(' ' , '')
 
-cur_dir = os.getcwd()
-
-inSystem = False
-
-file_list = os.listdir(cur_dir)
-
-for file in file_list:
-	if(file == file_name):
-		inSystem = True
-		break
+		newName = newName.lower()
 
 
-#not in the system
-if(inSystem == False): #
+		#finds the pickled file in project directory
+		file_name = newName.lower() + '.pickle'
 
-	newAge = int(input('What is your age?(yrs): '))
-	currWeight = float(input('What is your current weight?(lbs): '))
-	currHeight = float(input('What is your current height?(inches): '))
-	goalWeight = float(input('What is your goal weight?(lbs):  '))
-	activityLvl = float(input('What is your activity level? 1-5 from low to high): '))
-	desiredIntensity = float(input('What do you want your daily caloric deficit to be? \n 1000 \n 750 \n 500 \n '))
+		cur_dir = os.getcwd()
 
-	newProfile = Profile(newName,newAge,currWeight,currHeight,goalWeight,activityLvl,desiredIntensity)
+		inSystem = False
 
-	#adds a date key with the value of the currentweight in the dictionary 
-	newProfile.data[currentDate] = newProfile.currentWeight
+		file_list = os.listdir(cur_dir)
 
-	#updates all information about the person
-	passed = newProfile.calculateDailyCalories() 
+		for file in file_list:
+			if(file == file_name):
+				inSystem = True
+				break
 
-	#pickles the Profile which saves the data associated with it
-	pickle_out = open(newName + ".pickle", "wb")
-	pickle.dump(newProfile, pickle_out)
-	pickle_out.close()
+		if(inSystem == False):
+			mistake = int(input('\n Uh-oh no profile found. \n 1. Create a profile \n 2. Retype profile name \n'))
 
-#already in the system so no need to ask questions again
-else: 
+			if(mistake == 1):
+				profileLoop = False
+			else:
+				profileLoop = True
+		else:
+			profileLoop = False
 
-	#loads the user profile data
-	pickle_in = open(newName + ".pickle", "rb")
-
-	userProfile = pickle.load(pickle_in)
 	
 
-	print('You have a profile with us already', newName, '!')
+	#not in the system
+	if(inSystem == False): #
 
-	choice = int(input('What would you like to do? \n 1. Add daily weight \n 2. Visualize weight loss progress \n 3. Update Profile \n 4. Quit '))
+		newAge = int(input('What is your age?(yrs): '))
+		currWeight = float(input('What is your current weight?(lbs): '))
+		currHeight = float(input('What is your current height?(inches): '))
+		goalWeight = float(input('What is your goal weight?(lbs):  '))
+		activityLvl = float(input('What is your activity level? 1-5 from low to high): '))
+		desiredIntensity = float(input('What do you want your daily caloric deficit to be? \n 1000 \n 750 \n 500 \n '))
 
-	if(choice == 1):
+		newProfile = Profile(newName,newAge,currWeight,currHeight,goalWeight,activityLvl,desiredIntensity)
 
-		print('The date is currently', currentDate)
+		#adds a date key with the value of the currentweight in the dictionary 
+		newProfile.data[currentDate] = newProfile.currentWeight
 
-		print(userProfile.data)
-		weight = float(input('What is your current weight in lbs?'))
+		#updates all information about the person
+		passed = newProfile.calculateDailyCalories() 
 
-		#default value is to update the weight despite multiple weight entries on the same day
-		updateChoice = "y"
-		
-		#handles case of multiple weight entries on the same day
-		for key in userProfile.data:
-			print(key)
-			if(key == currentDate):
-				updateChoice = str(input("You have already input your weight for today, update it? (y/n)"))
-				break
-		
-		if(updateChoice == "y"):
-			userProfile.data[currentDate] = weight
-			userProfile.currentWeight = weight
-			pickle_out2 = open(userProfile.name + ".pickle", "wb")
-			pickle.dump(userProfile, pickle_out2)
-			pickle_out2.close()
-			print('Thanks! Recorded.')
+		#pickles the Profile which saves the data associated with it
+		pickle_outNewProfile = open(newName + ".pickle", "wb")
+		pickle.dump(newProfile, pickle_outNewProfile)
+		pickle_outNewProfile.close()
 
-		#do nothing if they decide to not update the current weight.
-
-	elif(choice == 2):
-
-		print('Loading Visualization!')
-
-		today = currentDate.strftime("%Y/%m/%d")
-
-		#sorts the keys (dateTime objects) in the dictionary.
-		sortedData = sorted(userProfile.data.items(), key = lambda d: d[0])
+		#the user has the option to go back and enter in another name or to have access to options bc already in the system.
 
 
-		plt.suptitle('Weight Loss Progress for ' + userProfile.name + ' as of ' + today, fontsize=14, fontweight='bold')
+	#already in the system so no need to ask questions again
+	
+	pickle_inProfile = open(newName + ".pickle", "rb")
 
-		#x are the keys(dateTime objects) and y are the weights
-		x,y = zip(*sortedData)
+	userProfile = pickle.load(pickle_inProfile)
 
-		plt.plot(x,y)
-		plt.ylabel('Weight (lbs)')
-		plt.xlabel('Time (days)')
-		plt.show()
+	print('Profile Login successful.')	
+	
+	subLoopRun = True
+
+	while subLoopRun:
+
+		choice = int(input('\n What would you like to do? \n 1. Add daily weight \n 2. Visualize weight loss progress \n 3. Update Profile \n 4. Back \n 5. Quit '))
+
+		if(choice == 1):
+
+			print('\n The date is currently ' + str(currentDate))
+
+			weight = float(input('\n What is your current weight in lbs?'))
+
+			#default value is to update the weight despite multiple weight entries on the same day
+			updateChoice = "y"
+			
+			#handles case of multiple weight entries on the same day
+			for key in userProfile.data:
+				if(key == currentDate):
+					updateChoice = str(input(" \n You have already input your weight for today, update it? (y/n)"))
+					break
+			
+			if(updateChoice == "y"):
+				userProfile.data[currentDate] = weight
+				userProfile.currentWeight = weight
+				pickle_out2 = open(userProfile.name + ".pickle", "wb")
+				pickle.dump(userProfile, pickle_out2)
+				pickle_out2.close()
+				print('\n Thanks! Recorded.')
+
+		elif(choice == 2):
+
+			print('Loading Visualization!')
+
+			today = currentDate.strftime("%Y/%m/%d")
+
+			#sorts the keys (dateTime objects) in the dictionary.
+			sortedData = sorted(userProfile.data.items(), key = lambda d: d[0])
+
+
+			plt.suptitle('Weight Loss Progress for ' + userProfile.name + ' as of ' + today, fontsize=14, fontweight='bold')
+
+			#x are the keys(dateTime objects) and y are the weights
+			x,y = zip(*sortedData)
+
+			plt.plot(x,y)
+			plt.ylabel('Weight (lbs)')
+			plt.xlabel('Time (days)')
+			plt.show()
+
+
+		elif(choice == 3):
+
+			
+			modifyLoopRun = True
+
+			while modifyLoopRun:
+
+				updateProfileChoice = int(input('\n 1. Modify weight of specified date \n 2. Delete a date and weight \n 3. Add a missed weight \n 4. Back \n 5. Quit '))
+
+			
+				#need to check if date is valid and in the dict first.
+
+				if(updateProfileChoice >= 1 and updateProfileChoice <= 3):
+
+					dateString = str(input('\n Please enter the desired date to be updated/deleted/added: (ex, 06/07/17) '))
+
+					updatedDateTime = datetime.datetime.strptime(dateString, '%m/%d/%y')
+
+					if(updateProfileChoice == 2):
+						userProfile.data.pop(updatedDateTime.date(), None) 
+					else:
+						updatedWeight = float(input('\n Please enter the new weight: '))
+
+						userProfile.data[updatedDateTime.date()] = updatedWeight	
+
+
+					pickle_outModify= open(userProfile.name + ".pickle", "wb")
+					pickle.dump(userProfile, pickle_outModify)
+					pickle_outModify.close()
+
+					print('\n Successfully changed!')
+
+				elif(updateProfileChoice == 4):
+					modifyLoopRun = False
+				elif(updateProfileChoice == 5):
+					exit()
+				else:
+					modifyLoopRun = True
+
+
+		elif(choice == 4):
+			subLoopRun = False
+		elif(choice == 5):
+			exit()
+		else:
+			subLoopRun = True
